c3eddf2819302bc06ebe79bf2ffb8c7f5102773e
Joanna Lee <joanna.lee56@gmail.com>
2021-12-09 15:58:38 -0800

    Finished first exercise of melons

---
+++ melons.py
@@ -0,0 +1,54 @@
+"""Classes for melon orders."""
+
+class AbstractMelonOrder:
+    """An abstract base class that other Melon Orders inherit from."""
+    def __init__(self, species, qty):
+        """Initialize melon order attributes. """
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+
+    def get_total(self):
+        """Calculate price, including tax."""
+        base_price = 7.5 if self.species == 'Christmas melon' else 5
+        total = (1 + self.tax) * self.qty * base_price
+        if self.order_type == 'international' and self.qty < 10:
+            total += 3
+        return total
+
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+        self.shipped = True
+
+    
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+    def __init__(self, species, qty):
+        self.order_type = "domestic"
+        self.tax = 0.08
+        super().__init__(species, qty)
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+    def __init__(self, species, qty, country_code):
+        self.order_type = "international"
+        self.country_code = country_code
+        self.tax = 0.17
+        super().__init__(species, qty)
+        
+
+    def get_country_code(self):
+        """Return the country code."""
+        return self.country_code
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    def __init__(self, species, qty):
+        self.tax = 0
+        self.passed_inspection = False
+        self.order_type = 'domestic'
+        super().__init__(species, qty)
+
+
+    def mark_inspection(self):
+        """Record the fact than an order has been shipped."""
+        self.passed_inspection = True
